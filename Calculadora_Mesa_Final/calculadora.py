8b70fe122cff4f254e593f62b263fa3316dab6ae
JUSTSAM3 <samuel02855@gmail.com>
2025-05-28 12:59:01 -0500

    El arreglo

--- Calculadora_Mesa_Final/calculadora.py
+++ Calculadora_Mesa_Final/calculadora.py
@@ -122,4 +122,4 @@ def index():
     return render_template('index.html', show_results=False)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
